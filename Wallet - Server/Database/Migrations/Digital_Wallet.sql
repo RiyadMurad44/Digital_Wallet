3c393d63808b4ad502e96564320c3b2de6923411
Riyad Murad <riyadmurad01@gmail.com>
2025-03-05 09:23:30 +0200

    Fixed some fields with the README file

--- Wallet - Server/Database/Migrations/Digital_Wallet.sql
+++ Wallet - Server/Database/Migrations/Digital_Wallet.sql
@@ -9,6 +9,14 @@ CREATE TABLE Admins (
     address TEXT
 );
 
+CREATE TABLE FAQ (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    admin_id INT UNIQUE,
+    FOREIGN KEY (admin_id) REFERENCES Admins(id) ON DELETE CASCADE
+);
+
 CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
